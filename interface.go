8d519fe9fe935b3a22d3357a6fc62505f255b8d1
Guilherme Alegre <guilherme.alegre@yonderland.com>
2024-03-25 17:53:30 +0000

    initial commit

--- interface.go
+++ interface.go
@@ -3,13 +3,13 @@ package state_machine
 type IStateMachine interface {
 	GetName() string
 	Load(filePath string) error
-	ProcessTransition(nextState string, obj any) error
+	ProcessTransition(nextState string, obj any) (success bool, err error)
 	AddCheckFunction(name string, handler HandlerFunc)
 	AddOnErrorFunction(name string, handler HandlerFunc)
 	AddOnSuccessFunction(name string, handler HandlerFunc)
 	AddExecuteFunction(handler HandlerExecFunction)
-	AddCurrentStateFunction(handler CurrenctStateFunc)
+	AddCurrentStateFunction(handler CurrentStateFunc)
 	AddStateMachineToTrigger(name string, stateMachine IStateMachine) IStateMachine
-	AddAdapterFunction(name string, handler HandlerAdaptersFunction)
+	AddAdapterFunction(name string, handler HandlerAdapterFunction)
 	AddFilterFunction(name string, handler HandlerFilterFunction)
 }
--- state-machine.go
+++ state-machine.go
@@ -1,81 +1,24 @@
 package state_machine
 
 import (
-	"fmt"
+	"bitbucket.org/asadventure/be-infrastructure-lib/errors"
 	"github.com/spf13/viper"
 	"os"
+	"regexp"
 	"strings"
 )
 
-type IStateMachine interface {
-	GetName() string
-	Load(filePath string) error
-	ProcessTransition(currentState, nextState string, obj any) error
-	runCheckFunction(handlers []string, obj any) error
-	runOnErrorFunction(handlers []string, obj any) error
-	runOnSuccessFunction(handlers []string, obj any) error
-	AddCheckFunction(name string, handler HandlerFunc)
-	AddOnErrorFunction(name string, handler HandlerFunc)
-	AddOnSuccessFunction(name string, handler HandlerFunc)
-	AddExecuteFunction(handler HandlerExecFunction)
-	getCheckFunction(name string) HandlerFunc
-	getOnErrorFunction(name string) HandlerFunc
-	getOnSuccessFunction(name string) HandlerFunc
-	AddStateMachineToTrigger(stateMachine IStateMachine) IStateMachine
-}
-
 func NewStateMachine() IStateMachine {
 	return &StateMachine{
-		MapStates:              make(map[string]map[string]Handlers),
-		StateMachinesToTrigger: make(map[string]IStateMachine),
-		CheckHandlers:          make(map[string]HandlerFunc),
-		OnSuccessHandlers:      make(map[string]HandlerFunc),
-		OnErrorHandlers:        make(map[string]HandlerFunc),
+		MapStates:                 make(map[string]map[string]Handlers),
+		stateMachinesToTriggerMap: make(map[string]IStateMachine),
+		CheckHandlers:             make(map[string]HandlerFunc),
+		OnSuccessHandlers:         make(map[string]HandlerFunc),
+		OnErrorHandlers:           make(map[string]HandlerFunc),
+		AdapterHandlers:           make(map[string]HandlerAdapterFunction),
+		FilterHandlers:            make(map[string]HandlerFilterFunction),
 	}
 }
-
-// StateMachine ...
-type StateMachine struct {
-	filePath               string
-	Name                   string                         `json:"name"`
-	States                 []State                        `json:"states"`
-	MapStates              map[string]map[string]Handlers `json:"map_states"`
-	Execute                HandlerExecFunction            `json:"execute"`
-	CheckHandlers          map[string]HandlerFunc         `json:"check_handlers"`
-	OnSuccessHandlers      map[string]HandlerFunc         `json:"on_success_handlers"`
-	OnErrorHandlers        map[string]HandlerFunc         `json:"on_error_handlers"`
-	StateMachinesToTrigger map[string]IStateMachine       `json:"state_machines_to_trigger"`
-}
-
-type State struct {
-	Name        string       `json:"name"`
-	Transitions []Transition `json:"transitions"`
-}
-
-type Transition struct {
-	// Name
-	Name string `json:"name"`
-	// Check
-	Check []string `json:"check"`
-	// On Success
-	OnSuccess []string `json:"on_success"  mapstructure:"on_success"`
-	// On Error
-	OnError []string `json:"on_error" mapstructure:"on_error"`
-}
-
-type Handlers struct {
-	//Update Status
-	updateStatus string
-	// Check
-	Check []string `json:"check"`
-	// On Success
-	OnSuccess []string `json:"on_success"`
-	// On Error
-	OnError []string `json:"on_error"`
-}
-
-type HandlerExecFunction func(currentState, nextState string, arg any) (success bool, err error)
-type HandlerFunc func(arg any, optArg ...string) (success bool, err error)
 
 func (sm *StateMachine) Load(filePath string) error {
 	// Read the JSON file
@@ -96,7 +39,6 @@ func (sm *StateMachine) Load(filePath string) error {
 		return err
 	}
 
-	var handlers Handlers
 	// initialize the state machine
 	for _, state := range sm.States {
 
@@ -105,13 +47,36 @@ func (sm *StateMachine) Load(filePath string) error {
 		}
 
 		for _, transition := range state.Transitions {
+			var handlers Handlers
 			// add check handlers
-			handlers.Check = append(handlers.Check, transition.Check...)
+			for _, check := range transition.Check {
+				funcName, args := splitFunctionAndArguments(check.Func)
+				handlers.Check = append(handlers.Check, CheckStruct{
+					Func:    funcName,
+					FuncArg: args,
+				})
+			}
 			// add on_success handlers
-			handlers.OnSuccess = append(handlers.OnSuccess, transition.OnSuccess...)
-
+			for _, onSuccess := range transition.OnSuccess {
+				funcName, args := splitFunctionAndArguments(onSuccess.Func)
+				handlers.OnSuccess = append(handlers.OnSuccess, OnSuccessStruct{
+					Func:            funcName,
+					FuncArg:         args,
+					Adapter:         onSuccess.Adapter,
+					Filter:          onSuccess.Filter,
+					IsStateMachine:  onSuccess.IsStateMachine,
+					IgnoreError:     onSuccess.IgnoreError,
+					IgnoreNoSuccess: onSuccess.IgnoreNoSuccess,
+				})
+			}
 			// add on_error handlers
-			handlers.OnError = append(handlers.OnError, transition.OnError...)
+			for _, onError := range transition.OnError {
+				funcName, args := splitFunctionAndArguments(onError.Func)
+				handlers.OnError = append(handlers.OnError, OnErrorStruct{
+					Func:    funcName,
+					FuncArg: args,
+				})
+			}
 
 			sm.MapStates[state.Name][transition.Name] = handlers
 		}
@@ -120,38 +85,8 @@ func (sm *StateMachine) Load(filePath string) error {
 	return nil
 }
 
-func checkIfFunctionWithArguments(input string) (success bool, function string, arguments []string) {
-	parts := strings.Split(input, "(")
-	if len(parts) != 2 {
-		return false, "", nil
-	}
-
-	functionName := parts[0]
-	argumentsString := parts[1]
-	argumentsString = strings.TrimSuffix(argumentsString, ")")
-	arguments = strings.Split(argumentsString, ",")
-	for i, arg := range arguments {
-		arguments[i] = strings.TrimSpace(arg)
-	}
-	return true, functionName, arguments
-}
-
 func (sm *StateMachine) GetName() string {
 	return sm.Name
-}
-
-func (sm *StateMachine) getCheckFunction(name string) HandlerFunc {
-	// it's an internal function
-	return sm.CheckHandlers[name]
-}
-func (sm *StateMachine) getOnErrorFunction(name string) HandlerFunc {
-	return sm.OnErrorHandlers[name]
-}
-
-func (sm *StateMachine) getOnSuccessFunction(name string) HandlerFunc {
-	// it's an internal function
-
-	return sm.OnSuccessHandlers[name]
 }
 
 func (sm *StateMachine) AddCheckFunction(name string, handler HandlerFunc) {
@@ -167,105 +102,190 @@ func (sm *StateMachine) AddOnSuccessFunction(name string, handler HandlerFunc) {
 }
 
 func (sm *StateMachine) AddExecuteFunction(handler HandlerExecFunction) {
-	sm.Execute = handler
+	sm.execute = handler
 }
 
-func (sm *StateMachine) runCheckFunction(handlers []string, obj any) error {
-	for _, handler := range handlers {
-		isIntFunc, funIntFunc, argIntFunc := checkIfFunctionWithArguments(handler)
-
-		if isIntFunc {
-			handler = funIntFunc
-		}
-
-		handlerFunc := sm.getCheckFunction(handler)
-
-		_, err := handlerFunc(obj, argIntFunc...)
-
-		if err != nil {
-			return err
-		}
-
-	}
-	return nil
-}
-
-func (sm *StateMachine) runOnErrorFunction(handlers []string, obj any) error {
-	for _, handler := range handlers {
-		handlerFunc := sm.getOnErrorFunction(handler)
-		_, err := handlerFunc(obj)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (sm *StateMachine) runOnSuccessFunction(handlers []string, obj any) error {
-	for _, handler := range handlers {
-		isFuncArg, nameFunc, argFunc := checkIfFunctionWithArguments(handler)
-
-		if smTrigger, isSMTrigger := sm.StateMachinesToTrigger[nameFunc]; isSMTrigger {
-			err := smTrigger.ProcessTransition(argFunc[0], argFunc[1], obj)
-			if err != nil {
-				return err
-			}
-
-		} else {
-			if isFuncArg {
-				handler = nameFunc
-			}
-			handlerFunc := sm.getCheckFunction(handler)
-
-			_, err := handlerFunc(obj, argFunc...)
-
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (sm *StateMachine) AddStateMachineToTrigger(stateMachine IStateMachine) IStateMachine {
-	sm.StateMachinesToTrigger[stateMachine.GetName()] = stateMachine
-
+func (sm *StateMachine) AddStateMachineToTrigger(name string, stateMachine IStateMachine) IStateMachine {
+	sm.stateMachinesToTriggerMap[name] = stateMachine
 	return sm
 }
 
-func (sm *StateMachine) ProcessTransition(currentState, nextState string, obj any) error {
+func (sm *StateMachine) AddAdapterFunction(name string, handler HandlerAdapterFunction) {
+	sm.AdapterHandlers[name] = handler
+}
 
+func (sm *StateMachine) AddFilterFunction(name string, handler HandlerFilterFunction) {
+	sm.FilterHandlers[name] = handler
+}
+
+func (sm *StateMachine) AddCurrentStateFunction(handler CurrentStateFunc) {
+	sm.currentState = handler
+}
+
+func (sm *StateMachine) ProcessTransition(nextState string, obj any) (success bool, err error) {
 	// Get handlers
+	currentState, err := sm.currentState(obj)
+	if err != nil {
+		return false, err
+	}
+
 	handlers, exitTransition := sm.MapStates[currentState][nextState]
 	if !exitTransition {
-		return fmt.Errorf("don't exit transition")
+		return false, errors.ErrorInStateMachineTransition().Formats(currentState, nextState, sm.Name)
 	}
 
-	err := sm.runCheckFunction(handlers.Check, obj)
+	success, err = sm.runCheckFunction(handlers.Check, obj)
 	if err != nil {
-		return err
+		if success, err = sm.runOnErrorFunction(handlers.OnError, obj); err != nil {
+			return success, err
+		}
+		return success, err
 	}
 
-	success, err := sm.Execute(currentState, nextState, obj)
+	if !success {
+		return false, nil
+	}
+
+	err = sm.execute(nextState, obj)
 	if err != nil {
-		return err
+		if success, err = sm.runOnErrorFunction(handlers.OnError, obj); err != nil {
+			return success, err
+		}
+		return success, err
 	}
 
-	if success {
-		err := sm.runOnSuccessFunction(handlers.OnSuccess, obj)
-		if err != nil {
-			return err
+	success, err = sm.runOnSuccessFunction(handlers.OnSuccess, obj)
+	if err != nil {
+		return sm.runOnErrorFunction(handlers.OnError, obj)
+	}
+
+	return success, nil
+}
+
+func (sm *StateMachine) getCheckFunction(name string) HandlerFunc {
+	// it's an internal function
+	return sm.CheckHandlers[name]
+}
+func (sm *StateMachine) getOnErrorFunction(name string) HandlerFunc {
+	return sm.OnErrorHandlers[name]
+}
+
+func (sm *StateMachine) getOnSuccessFunction(name string) HandlerFunc {
+	return sm.OnSuccessHandlers[name]
+}
+
+func (sm *StateMachine) getAdapterFunction(name string) HandlerAdapterFunction {
+	return sm.AdapterHandlers[name]
+}
+
+func (sm *StateMachine) getFilterFunction(name string) HandlerFilterFunction {
+	return sm.FilterHandlers[name]
+}
+
+func (sm *StateMachine) getStateMachineToTrigger(name string) IStateMachine {
+	return sm.stateMachinesToTriggerMap[name]
+}
+
+func (sm *StateMachine) runCheckFunction(handlers []CheckStruct, obj any) (success bool, err error) {
+	success = true
+	for _, handler := range handlers {
+		handlerFunc := sm.getCheckFunction(handler.Func)
+
+		success, err = handlerFunc(obj, handler.FuncArg...)
+		if err != nil && !handler.IgnoreError {
+			return false, err
+		}
+
+		if !success && !handler.IgnoreNoSuccess {
+			return false, errors.ErrorInStateMachineTransition()
 		}
 	}
 
-	defer func() {
-		if err != nil {
-			err := sm.runOnErrorFunction(handlers.OnError, obj)
+	return success, nil
+}
+
+func (sm *StateMachine) runOnErrorFunction(handlers []OnErrorStruct, obj any) (bool, error) {
+	for _, handler := range handlers {
+		handlerFunc := sm.getOnErrorFunction(handler.Func)
+		success, err := handlerFunc(obj)
+		if err != nil && !handler.IgnoreError {
+			return false, err
+		}
+
+		if !success && !handler.IgnoreNoSuccess {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func (sm *StateMachine) runOnSuccessFunction(handlers []OnSuccessStruct, obj any) (bool, error) {
+	for _, handler := range handlers {
+		objs := []any{obj}
+
+		adapter := sm.getAdapterFunction(handler.Adapter)
+		if adapter != nil {
+			newObjs, err := adapter(obj)
 			if err != nil {
-				return
+				return false, err
+			}
+			objs = newObjs
+		}
+
+		filter := sm.getFilterFunction(handler.Filter)
+		if filter != nil {
+			newObjs, err := filter(objs)
+			if err != nil {
+				return false, err
+			}
+			objs = newObjs
+		}
+
+		for _, obj := range objs {
+			if handler.IsStateMachine {
+				smTrigger := sm.getStateMachineToTrigger(handler.Func)
+				if smTrigger != nil {
+					success, err := smTrigger.ProcessTransition(handler.FuncArg[1], obj)
+					if err != nil && !handler.IgnoreError {
+						return false, err
+					}
+
+					if !success && !handler.IgnoreNoSuccess {
+						return false, nil
+					}
+				}
+			} else {
+				handlerFunc := sm.getOnSuccessFunction(handler.Func)
+				success, err := handlerFunc(obj, handler.FuncArg...)
+				if err != nil && !handler.IgnoreError {
+					return false, err
+				}
+
+				if !success && !handler.IgnoreNoSuccess {
+					return false, nil
+				}
 			}
 		}
-	}()
+	}
 
-	return nil
+	return true, nil
+}
+
+func splitFunctionAndArguments(input string) (function string, arguments []string) {
+	pattern := regexp.MustCompile(`^[a-zA-Z0-9_-]+(?:-[a-zA-Z0-9_-]+)?\((?:[a-zA-Z0-9_-]+(?:-[a-zA-Z0-9_]+)?)*(,\s*[a-zA-Z0-9_-]+(?:-[a-zA-Z0-9_]+)*)?\)$`)
+	if !pattern.MatchString(input) {
+		return input, nil
+	}
+
+	parts := strings.Split(input, "(")
+	functionName := parts[0]
+	argumentsString := parts[1]
+	argumentsString = strings.TrimSuffix(argumentsString, ")")
+	arguments = strings.Split(argumentsString, ",")
+	for i, arg := range arguments {
+		arguments[i] = strings.TrimSpace(arg)
+	}
+
+	return functionName, arguments
 }
